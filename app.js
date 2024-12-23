bc417bfebb9da9325e848bc22d7e76d8019ed6fa
Austin <austin.moorer@gmail.com>
2024-12-23 16:01:00 -0500

    Big commit, adding components and auth working

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require("node:path");
 const { Pool } = require("pg");
 const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
 const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session);
 const passport = require("passport");
@@ -31,21 +30,12 @@ app.use(
 );
 
 app.use(passport.session());
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "/public")));
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-      id,
-    ]);
-    const user = rows[0];
-
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
 });
 
 passport.use(
@@ -70,21 +60,36 @@ passport.use(
     }
   }),
 );
+passport.serializeUser((user, done) => {
+  done(null, user.userid);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE userid = $1", [
+      id,
+    ]);
+    const user = rows[0];
 
-app.use(express.urlencoded({ extended: true }));
-app.use(expressLayouts);
-app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
-  next();
+    done(null, user);
+  } catch (err) {
+    console.log(err);
+    done(err);
+  }
 });
 
-app.get("/", (req, res) => {
-  res.render("index", { user: req.user });
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
 });
 
 app.get("/sign-up", (req, res) => res.render("sign-up"));
 app.post("/sign-up", async (req, res, next) => {
   try {
+    const newUser = {
+      username: req.body.username,
+      fName: req.body.fName,
+      lName: req.body.lName,
+    };
     const existingUser = await pool.query({
       text: `
         SELECT
@@ -93,23 +98,31 @@ app.post("/sign-up", async (req, res, next) => {
           users
         WHERE
           username = $1`,
-      values: [req.body.username],
+      values: [newUser.username],
     });
-    if (existingUser) {
-      return res.render("sign-up", { error: "Username already exists" });
+    if (existingUser.rows[0]) {
+      return res.render("sign-up", {
+        error: "Username already  exists",
+        newUser: newUser,
+      });
     }
     if (req.body.password !== req.body.confirm) {
-      return res.render("sign-up", { error: "Passwords do not match" });
+      return res.render("sign-up", {
+        error: "Passwords do not match",
+        newUser: newUser,
+      });
     }
     bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
       if (err) throw err;
       await pool.query(
-        "INSERT INTO users (username, password) VALUES ($1, $2)",
-        [req.body.username, hashedPassword],
+        "INSERT INTO users (username, password, fname, lname, membership, admin) VALUES ($1, $2, null, null, false, false)",
+        [newUser.username, hashedPassword],
       );
     });
     res.redirect("/");
   } catch (err) {
+    console.log(err);
+    res.redirect("/");
     return next(err);
   }
 });
@@ -119,6 +132,7 @@ app.post(
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/",
+    failureMessage: "Incorrect username or password",
   }),
 );
 
@@ -129,6 +143,27 @@ app.get("/log-out", (req, res, next) => {
     }
     res.redirect("/");
   });
+});
+
+app.post("/upgrade", async (req, res) => {
+  if (req.body.secret === process.env.validation) {
+    await pool.query({
+      text: `
+          UPDATE
+            users
+          SET
+            membership = true
+          WHERE
+            id = $1`,
+      values: [req.body.id],
+    });
+  } else {
+    res.render("index", { user: req.user, error: "Sucks to suck" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user, error: req.session.messages });
 });
 
 app.listen(3000, () =>
